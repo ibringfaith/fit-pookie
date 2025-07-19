e9002d1b29d0368088e762ddbc02b81b5a24fbcb
Katherine Awad <ka1014@princeton.edu>
2025-07-18 22:08:02 -0400

    Added new button component specifically for popups to allow custom centering. Note that for now this custom centering is hard coded, since all the popups should look similar. Turned off the blue highlight that surrounds the popup button.

--- frontend/src/components/PageOne.jsx
+++ frontend/src/components/PageOne.jsx
@@ -1,4 +1,4 @@
-import RightArrowButton from './RightArrowButton.jsx'
+import { PopupArrowButton } from './PopupArrowButton.jsx'
 import { useRef, useEffect } from 'react'
 
 export function PageOne()
@@ -16,18 +16,21 @@ export function PageOne()
     <>
       {/* <div className = "flex justify-center items-center"> */}
         <dialog ref={dialogRef}>
-          <div className="grid-rows-2 content-center size-[500px] bg-no-repeat bg-contain bg-[url('/text_box.png')]">
-            <h3 className="py-2">Welcome!</h3>
-            <p className="text-balance text-wrap">
-            This is fit pookie, a web app  <br></br>
-            where you pull the slot  <br></br>
-            machine to pick an outfit. </p>
-            <br></br>
-            <p className="text-balance text-wrap"> 
-            Before we make our first pull, let's<br></br>
-            start by clicking the yes button below. <br></br>
-            This will take us to the closet where <br></br>
-            we can add our first outfit pieces! </p>
+          <div className="grid-rows-3 content-evenly content-center size-[500px] bg-no-repeat bg-contain bg-[url('/text_box.png')]">
+            <div>
+              <h3 className="py-2">Welcome!</h3>
+            </div>
+            <div>
+              <p className="text-balance text-wrap">
+              This is fit pookie, a web app  <br></br>
+              where you pull the slot  <br></br>
+              machine to pick an outfit. <br></br><br></br>
+              Before we make our first pull, let's<br></br>
+              start by clicking the yes button below. <br></br>
+              This will take us to the closet where <br></br>
+              we can add our first outfit pieces!
+              </p>
+            </div>
             {/* To make your first pull, you need to add outfit
             pieces to your wardrobe. You can do this by clicking
             the hanger icon, then 
@@ -38,10 +41,9 @@ export function PageOne()
             machine. If you want to save the fit <br></br>
             to your gallery, click the save icon. 
             */}
-            <RightArrowButton isInvisible={false} text = "OK" textSize = "text-[2em]" arrowSize= "size-1/14" padSize="py-5 px-2" onClick={() => dialogRef.current.close()}/>
+            <PopupArrowButton isInvisible={true} text = "OK" textSize = "text-[1.75em]" isRight = {true} iconSize= "w-1/28 h-1/4" onClick={() => dialogRef.current.close()}/>          
           </div>
         </dialog>
-      {/* </div>    */}
     </>
   )
 }
---
+++ frontend/src/components/PopupArrowButton.jsx
@@ -0,0 +1,36 @@
+import triangleRight from '../assets/triangle_right.png'
+import triangleLeft from '../assets/triangle_left.png'
+
+export function PopupArrowButton({ isInvisible, onClick, text, textSize, isRight = true, iconSize })
+{
+  let icon = null
+  let classStr = ""
+  let classNameStr = "absolute left-[225px] " + textSize 
+
+  if (isInvisible)
+    {
+      classStr="absolute left-[197px] bottom-[36px] invisible group-hover:visible " + iconSize 
+    }
+  else
+    {
+      classStr= "absolute left-[197px] bottom-[36px] " + iconSize
+    }
+
+    if (isRight)
+    {
+        icon = triangleRight
+    }
+    else
+    {
+        icon = triangleLeft
+    }
+
+    return (
+      <div className="relative h-[80px] py-2 group">
+        <button className={classNameStr} onClick={onClick}>
+            {text}
+        </button>
+        <img className={classStr} src={icon}/>
+      </div>
+    )
+}
