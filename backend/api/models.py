0c614acf0bceb3778225d6887a2f9cef22e1a0bd
Faith Best <97316553+ibringfaith@users.noreply.github.com>
2025-07-22 03:21:11 +0000

    create model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Clothing(models.Model):
+    type = models.CharField(max_length=20)
+    date = models.DateTimeField(auto_now_add=True)
+    item = models.ImageField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE, elated_name="clothes") #default=None 
+
+    def __str__(self):
+        return self.type
