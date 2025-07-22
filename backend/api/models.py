b31c058790e4ad7a2139b7180831ffb4c0c30c67
Faith Best <97316553+ibringfaith@users.noreply.github.com>
2025-07-22 03:50:44 +0000

    update models and modules

--- backend/api/models.py
+++ backend/api/models.py
@@ -7,7 +7,7 @@ class Clothing(models.Model):
     type = models.CharField(max_length=20)
     date = models.DateTimeField(auto_now_add=True)
     item = models.ImageField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE, elated_name="clothes") #default=None 
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="clothes") #default=None 
 
     def __str__(self):
         return self.type
