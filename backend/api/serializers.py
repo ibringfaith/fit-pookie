14a03001cf24465584aa2afa1684983a1d1308d6
Faith Best <97316553+ibringfaith@users.noreply.github.com>
2025-07-22 03:24:24 +0000

    create serializer for clothing model

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from .models import Clothing
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,3 +11,9 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+    
+class ClothingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Clothing
+        fields = ["id", "type", "image", "date", "author"]
+        extra_kwargs = {"author": {"read_only": True}}
